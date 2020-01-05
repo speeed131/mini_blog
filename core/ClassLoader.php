4ed01f8f8d16691fc06a8c9ed89ffeb87f410972
Daiki Hirose <programer.sd131@gmail.com>
2020-01-05 12:42:06 +0900

    add file in web

--- core/ClassLoader.php
+++ core/ClassLoader.php
@@ -6,18 +6,22 @@
     loadClass: オートロードが実行された際にクラスファイルを読み込む処理
 */
 
-class ClassLoader{
+class ClassLoader
+{
     protected $dirs;
 
-    public function register(){
+    public function register()
+    {
         spl_autoload_register(array($this, 'loadClass')); //引数に関数を指定 → コールバック関数
     }
 
-    public function registerDir($dir){
+    public function registerDir($dir)
+    {
         $this->dirs[] = $dir;
     }
 
-    public function loadClass($class){
+    public function loadClass($class)
+    {
         foreach ($this->dirs as $dir) {
             $file = $dir . '/' . $class . '.php';
             if(is_readable($file)){
