2ee0411b1c961c9154cba7aa7c096e2bcef8a351
Daiki Hirose <programer.sd131@gmail.com>
2020-01-05 15:59:00 +0900

    add URL制御

--- core/Request.php
+++ core/Request.php
@@ -1,5 +1,10 @@
 <?php
 
+/*
+    ユーザのリクエスト情報を制御するクラス。
+    主な機能としては、HTTPメソッド（GET/POST)の判定、値の取得、URLの取得、フロントコントローラ採用のためURL情報の制御
+*/
+
 class Request
 {
     public function isPost()
@@ -55,5 +60,44 @@ class Request
     public function getRequestUri()
     {
         return $_SERVER['REQUEST_URI'];
+    }
+
+
+/*
+    REQUEST_URI は、 ホスト部分よりあとの全ての値
+    SCRIPT_NAME は、 フロントコントローラ（index.php)までの値
+*/
+
+    public function getBaseUrl()
+    {
+        $script_name = $_SERVER['SCRIPT_NAME'];
+
+        $request_uri = $this->getRequestUri();
+
+        if(0 === strpos($request_uri, $script_name)) //URLにindex.phpが含まれている場合(true)
+        {
+            return $script_name;
+        }
+        elseif (0 === strpos($request_uri, dirname($script_name))) 
+        {
+            return rtrim(dirname($script_name), '/'); 
+        }
+
+        return '';
+    }
+
+    public function getPathInfo()
+    {
+        $base_url = $this->getBaseUrl();
+        $request_uri = $this->getRequestUri();
+
+        if(false !== ($pos = strpos($request_uri, '?'))) //$request_uri に ? のパラメータが含まれていたら、それより前を$request_uriにする
+        {
+            $request_uri = substr($request_uri, 0, $pos);
+        }
+
+        $path_info = (string)substr($request_uri, strlen($base_url)); //$request_uri から $base_url を除いて、 $path_info に代入
+
+        return $path_info;
     }
 }
--- web/index.php
+++ web/index.php
@@ -1,3 +1,16 @@
 <?php
 
+/*
+    いわゆるフロントコントローラにあたる部分。全てのリクエストをこのファイルで受け取る。
+
+＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+    core/.htaccess について
+        Apacheの設定を変更するファイル。
+        example.com/list のURLは、
+        example.com/index.php/list でアクセス時と同じ
+*/
+
 require '../bootstrap.php';
+
+
+
