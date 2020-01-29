4741e7b04e51093c860402ad39b0576b519f974c
Daiki Hirose <programer.sd131@gmail.com>
2020-01-29 20:40:32 +0900

    add resolve method in Router.php

--- core/Router.php
+++ core/Router.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+    Routerクラス。ルーティング定義配列とPATH_INFOを受け取り、ルーティングパラメータを特定する。
+
+*/
 class Router
 {
     protected $routes;
@@ -9,6 +13,7 @@ class Router
         $this->routes = $this->compileRoutes($definitions);
     }
 
+    //受け取ったルーティング定義配列中の動的パラメータ指定を正規表現で扱える形式に変換する
     public function compileRoutes($definitions)
     {
         $routes = array();
@@ -33,6 +38,7 @@ class Router
         return $routes;
     }
 
+    //定義済みのルーティング定義配列とPATH_INFOのマッチングで、ルーティングパラメータの特定を行う。
     public function resolve($path_info)
     {
         if('/' !== substr($path_info, 0, 1)){
