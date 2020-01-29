cec78b58633643897b2ec2012a56c3836b54c1be
Daiki Hirose <programer.sd131@gmail.com>
2020-01-29 20:15:03 +0900

    add resolve method in Router.php

--- core/Router.php
+++ core/Router.php
@@ -14,17 +14,39 @@ class Router
         $routes = array();
 
         foreach ($definitions as $url => $params) {
-            $tokens = explode('/', ltrim($url, '/'))
+            //explode関数・・・左で右を分割
+            //ltrim関数・・・左から右を取り除く
+            $tokens = explode('/', ltrim($url, '/'));
             foreach ($tokens as $i => $token) {
+                //strpos・・・左から右の位置を数字で返す（0スタート）
                 if(0 === strpos($token, ':')){
                     $name = substr($token, 1);
                     $token = '(?p<' . $name . '>[^/]+)';
                 }
                 $tokens[$i] = $token;
             }
+            //implode関数・・・左で右を連結する
             $pattern = '/' . implode('/', $tokens);
             $routes[$pattern] = $params;
         }
+
+        return $routes;
     }
 
+    public function resolve($path_info)
+    {
+        if('/' !== substr($path_info, 0, 1)){
+            $path_info = '/' . $path_info;
+        }
+
+        foreach ($this->routes as $pattern => $params) {
+            if (preg_match('#^' . $pattern . '$#', $path_info, $matches)) {
+                $params = array_merge($params, $matches);
+
+                return $params;
+            }
+        }
+
+        return false;
+    }
 }
