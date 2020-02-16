a2fcdacaadd6f928cbb439cb590dfa7859b79731
Daiki Hirose <programer.sd131@gmail.com>
2020-02-16 16:52:58 +0900

    first commit

---
+++ MiniblogApplication.php
@@ -0,0 +1,27 @@
+<?php
+
+class MiniBlogApplication extends Application
+{
+    protected $login_action = array('account', 'signin');
+
+    public function getRootDir()
+    {
+        return dirname(__FILE__);
+    }
+
+    protected function registerRoutes()
+    {
+        return array(
+
+        );
+    }
+
+    protected function configure()
+    {
+        $this->db_manager->connnet('master', array(
+            'dsn'      => 'mysql:dbname=mini_blog; host=localhost',
+            'user'     => 'root',
+            'password' => 'root',
+        ));
+    }
+}
--- web/index_dev.php
+++ web/index_dev.php
@@ -14,7 +14,7 @@
 require '../bootstrap.php';
 require '../MiniblogApplication.php';
 
-$app = new MiniBlogApplication(false);
+$app = new MiniBlogApplication(true);
 $app->run();
 
 
